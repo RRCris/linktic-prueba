385a99fc623827d70daea2d450b5dc881a4b8674
RRCris <curiosity.cdrr@gmail.com>
2025-04-23 05:00:34 -0500

    feat: añadir animacion a nuevos items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import { useAPI } from '../composables/useAPI';
 import { API_MAP } from '../constants/apiRoutes';
 import { computed, onMounted, ref } from 'vue';
 
-
+const nameFilter = ref('')
 const page = ref(1)
 const router = useRouter()
 const { path, schema } = API_MAP.character
@@ -40,11 +40,13 @@ const goNextPage = () => {
 
 
 const itemsLoadings = computed(() => accumulative.value.map(dt => dt.results).flat())
+const startDelay = computed(() => itemsLoadings.value.length - 20)
 
 
 </script>
 
 <template>
+    <input type="text" v-model="nameFilter" />
     <div v-if="isLoading && accumulative.length === 0">Cargando...</div>
     <div v-else-if="isError">Error: {{ errorMessage }}</div>
     <div v-else-if="itemsLoadings">
@@ -53,13 +55,18 @@ const itemsLoadings = computed(() => accumulative.value.map(dt => dt.results).fl
         <p>Pagina {{ page }}</p>
         <p>{{ itemsLoadings.length }}</p>
         <ul>
-            <li v-for="character in itemsLoadings" :key="character.id" @click="goToDetails(character.id)">
-                <img :src="character.image" :alt="character.name" width="80" />
-                <p>{{ character.name }}</p>
-            </li>
+            <TransitionGroup name="slide-bounce" tag="ul">
+                <li v-for="(character, i) in itemsLoadings" :key="character.id" @click="goToDetails(character.id)"
+                    :style="{ '--delay': `${(i - startDelay) * 160}ms` }">
+                    <img :src="character.image" :alt="character.name" width="80" />
+                    <p>{{ character.name }}</p>
+                </li>
+            </TransitionGroup>
         </ul>
     </div>
-    <div ref="observerTarget" style="height: 40px;" />
+    <div ref="observerTarget" style="height: 40px; margin-top: 20px;" />
+    <div v-if="isLoading">Cargando más...</div>
+
 
 
 </template>
@@ -76,5 +83,40 @@ li {
     gap: 10px;
     cursor: pointer;
     margin-bottom: 12px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.8s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.slide-bounce-enter-active {
+    animation: slideInBounce 0.6s ease-out both;
+    animation-delay: var(--delay);
+}
+
+@keyframes slideInBounce {
+    0% {
+        opacity: 0;
+        transform: translateX(100%);
+    }
+
+    60% {
+        opacity: 1;
+        transform: translateX(-10px);
+    }
+
+    80% {
+        transform: translateX(5px);
+    }
+
+    100% {
+        transform: translateX(0);
+    }
 }
 </style>
